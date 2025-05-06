9bbc76a0fbd70a8d52ef2987496d6cf73cc8c868
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-05-06 15:18:13 +0700

    Membuat beberapa pointer yang dapat di access dan di arrow oleh member

--- Pointer1.cpp
+++ Pointer1.cpp
@@ -10,3 +10,18 @@ class mahasiswa
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // Member access operator
+
+    mahasiswa &refMhs = mhs; // Pointer reference refMhs
+    refMhs.nim = 2; // Member access operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer reference pMhs
+    pMhs->nim = 3; // Arrow operator
+    pMhs->showNim();
+    return 0;
+}
